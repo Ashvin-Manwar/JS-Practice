06f881ab07c93d6d9d82b5094c1446613abbc2d4
Ashvin Manwar <manwar.ashvin@gmail.com>
2024-05-25 14:11:11 +0000

    datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+// console.log(3+3) // code readability should be high
+// console.log("Ashvin")
+
+let name = "Ashvin"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// null => standalone value
+// bigint
+// string => ""
+// symbol => unique
+// boolean => true/false
+// undefined => 
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,13 @@
-console.log("Ashvin")
+// console.log("Ashvin")
+
+const accountId=12355
+let accountEmail="ashvin@gmail.com"
+var accountPassword ="123456789"
+accountCity= "RAJKOT"
+let accountState;
+
+accountEmail="adi@gmail.com"
+accountPassword="21212121"
+accountCity="yavatmal"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
