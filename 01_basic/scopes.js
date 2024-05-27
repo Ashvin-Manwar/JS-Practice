ed8175934b020317da9161c6f6df5853223904b3
Ashvin-Manwar <manwar.ashvin.manwar@gmail.com>
2024-05-27 18:16:56 +0530

    Scope

---
+++ 01_basic/scopes.js
@@ -0,0 +1,46 @@
+// var c = 300
+// let a =300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // var c = 300
+//     console.log("INNER: ", a);    
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "ashvin"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+     two()
+}
+// one()
+
+// if (true) {
+//     const username = "ashvin"
+//     if (username === "ashvin") {
+//         const website = " youtube"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
+
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
